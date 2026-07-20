890d5e5d6ea5bb3f921b318ddede3e2d519521e7
井出実莉 <ideminori@jm-MacBook-Air.local>
2026-07-21 02:33:23 +0900

    Initial commit: Astro blog scaffold for 家計簿＋

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '家計簿＋';
+export const SITE_DESCRIPTION = '家計管理や節約のヒントを綴るブログです。';
